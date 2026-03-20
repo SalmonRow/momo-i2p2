039ddab335638b6c1f734a017038c92d3a5860a0
My name <159037321+SalmonRow@users.noreply.github.com>
2026-03-20 16:24:21 +0800

    water dis bfs and cortisol

--- hw/lab2/cortisol.c
+++ hw/lab2/cortisol.c
@@ -3,6 +3,25 @@
 
 int main()
 {
-
+    int n, q;
+    scanf("%d", &n);
+    int cortisols[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &cortisols[i]);
+    }
+    scanf("%d", &q);
+    for (int j = 0; j < q; j++)
+    {
+        int a, b;
+        scanf("%d %d", &a, &b);
+        int stress = 0;
+        while (a <= b)
+        {
+            stress += cortisols[a];
+            a++;
+        }
+        printf("%d\n", stress);
+    }
     return 0;
 }
--- hw/lab2/water_dis.c
+++ hw/lab2/water_dis.c
@@ -1,8 +1,57 @@
 #include <stdio.h>
-#include <stdlib.h>
+#define MAXN 105
+
+// globals
+int grid[MAXN][MAXN];
+int visited[MAXN][MAXN];
+int qx[MAXN * MAXN];
+int qy[MAXN * MAXN];
+int dx[] = {1, -1, 0, 0};
+int dy[] = {0, 0, 1, -1};
+
+int bfs(int n, int m)
+{
+    int head = 0, tail = 0, count = 0;
+    qx[tail] = 0;
+    qy[tail] = 0;
+    tail++;
+    visited[0][0] = 1;
+    count++;
+
+    while (head < tail)
+    {
+        int cx = qx[head];
+        int cy = qy[head];
+        head++;
+
+        for (int j = 0; j < 4; j++)
+        {
+            int nx = cx + dx[j];
+            int ny = cy + dy[j];
+            if (ny >= 0 && ny < m && nx >= 0 && nx < n && !visited[nx][ny] && grid[nx][ny] <= grid[cx][cy])
+            {
+                visited[nx][ny] = 1;
+                qx[tail] = nx;
+                qy[tail] = ny;
+                tail++;
+                count++;
+            }
+        }
+    }
+    return count;
+}
 
 int main()
 {
-
+    int n, m;
+    scanf("%d %d", &n, &m);
+    for (int i = 0; i < n; i++)
+    {
+        for (int ii = 0; ii < m; ii++)
+        {
+            scanf("%d", &grid[i][ii]);
+        }
+    }
+    printf("%d\n", bfs(n, m));
     return 0;
 }
